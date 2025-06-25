7011d6090f659021f6b3f95f8a652ecd5ff7caf3
SALMA HARDA <salmaharda@gmail.com>
2025-06-25 11:24:09 +0100

    add daily challange

---
+++ week 2/Day 1/Daily Challenge/Actors.sql
@@ -0,0 +1,17 @@
+/*
+CREATE TABLE actors (
+    actor_id SERIAL PRIMARY KEY,
+    first_name TEXT ,
+    last_name TEXT 
+  
+
+
+SELECT COUNT(*) FROM actors;
+
+
+INSERT INTO actors (first_name, last_name)
+VALUES (NULL, 'rek');
+
+*/
+
+
